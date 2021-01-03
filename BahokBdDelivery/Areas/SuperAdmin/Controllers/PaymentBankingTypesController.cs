d3cfdb3cc96a54beaa78599b32377828e4bcd647
pranka mondal <priankanew0011@gmail.com>
2021-01-04 00:11:07 +0600

    .post

--- BahokBdDelivery/Areas/SuperAdmin/Controllers/PaymentBankingTypesController.cs
+++ BahokBdDelivery/Areas/SuperAdmin/Controllers/PaymentBankingTypesController.cs
@@ -70,44 +70,6 @@ namespace BahokBdDelivery.Areas.SuperAdmin.Controllers
             }
 
         }
-
-        // POST: SuperAdmin/PaymentBankingTypes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost("/PaymentBankingTypes/postPayType")]
-        [DisableRequestSizeLimit]
-        public async Task<IActionResult> Create(PayMethodVm postItem)
-        {
-            if (ModelState.IsValid)
-            {
-                PaymentBankingType entity = new PaymentBankingType();
-                entity.Id = Guid.NewGuid();
-                entity.BankingMethodName = postItem.BankingMethodName;
-                _context.Add(entity);
-                await _context.SaveChangesAsync();
-                // return RedirectToAction(nameof(Index));
-            }
-            return View(postItem);
-        }
-        // GET: SuperAdmin/PaymentBankingTypes/Edit/5
-        public async Task<IActionResult> Edit(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var paymentBankingType = await _context.PaymentBankingType.FindAsync(id);
-            if (paymentBankingType == null)
-            {
-                return NotFound();
-            }
-            return View(paymentBankingType);
-        }
-
-        // POST: SuperAdmin/PaymentBankingTypes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddOrEdit(Guid id, PaymentBankingType paymentBankingType)
